4cac7937688cfd5d04776b46d4a6296f767b2443
Ghost4Wandering <yyunjie9@163.com>
2018-01-06 20:54:30 +0800

    切分功能模块到Controller
    切分功能模块到Controller

--- src/main/java/com/mmnttech/ma/merchant/server/controller/ComplaintController.java
+++ src/main/java/com/mmnttech/ma/merchant/server/controller/ComplaintController.java
@@ -1,8 +1,18 @@
 package com.mmnttech.ma.merchant.server.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mmnttech.ma.merchant.server.common.entity.QueryEntity;
+import com.mmnttech.ma.merchant.server.common.entity.RtnMessage;
 import com.mmnttech.ma.merchant.server.service.ComplaintService;
 
 /**
@@ -21,5 +31,25 @@ public class ComplaintController {
 
 	@Autowired
 	private ComplaintService complaintService;
+
+	private Logger logger = LoggerFactory.getLogger(ComplaintController.class);
+	
+	//商户投诉查询
+	@ResponseBody
+	@RequestMapping(value = "queryComplaintInfos")
+	public RtnMessage queryComplaintInfos(HttpServletRequest request, HttpServletResponse response,
+			@ModelAttribute("queryEntity") QueryEntity queryEntity) {
+		RtnMessage rtnMsg = new RtnMessage();
+		try {
+			//TODO
+			
+		} catch (Exception e) {
+			logger.error("queryComplaintInfos 出现异常：", e);
+			rtnMsg.setIsSuccess(false);
+			rtnMsg.setMessage("商户投诉查询：请稍后再试");
+		}
+		
+		return rtnMsg;
+	}
 	
 }
