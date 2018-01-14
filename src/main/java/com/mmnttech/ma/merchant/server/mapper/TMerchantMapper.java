e6a1738bcc443106d32b2b46939318733406cced
Stelo <kkcyc1996@gmail.com>
2018-01-14 21:15:42 +0800

    [Imp]调整项目结构

--- src/main/java/com/mmnttech/ma/merchant/server/mapper/TMerchantMapper.java
+++
@@ -1,7 +0,0 @@
-package com.mmnttech.ma.merchant.server.mapper;
-
-import com.mmnttech.ma.merchant.server.model.TMerchant;
-import com.mmnttech.ma.merchant.server.util.BaseMapper;
-
-public interface TMerchantMapper extends BaseMapper<TMerchant> {
-}
--- src/main/java/com/mmnttech/ma/merchant/server/mapper/TMerchantUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.mmnttech.ma.merchant.server.mapper;
-
-import com.mmnttech.ma.merchant.server.model.TMerchantUser;
-import com.mmnttech.ma.merchant.server.util.BaseMapper;
-
-public interface TMerchantUserMapper extends BaseMapper<TMerchantUser> {
-}
